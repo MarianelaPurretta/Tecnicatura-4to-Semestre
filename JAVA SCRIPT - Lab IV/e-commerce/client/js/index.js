f2b28aa68860934606dd3a4948b0b5dd72475ca6
Marianela <135768183+MarianelaPurretta@users.noreply.github.com>
2024-08-28 16:57:50 -0300

    Avances E-commerce Header

--- JAVA SCRIPT - Lab IV/e-commerce/client/js/index.js
+++ JAVA SCRIPT - Lab IV/e-commerce/client/js/index.js
@@ -1,24 +1,31 @@
-const shopContent = document.getElementById("shopContent");
+
+const cart = []; //carrito
 
 productos.forEach((product) => {
     const content = document.createElement("div");
+    content.classList.add("card-products"); // Asegúrate de que tenga la clase correcta
+
     content.innerHTML = `
-    <img src="${product.img}">
-    <h3>${product.productName}</h3>
-    <p>${product.price}</p>
+        <img src="${product.img}">
+        <h3>${product.productName}</h3>
+        <p>${product.price}</p>
     `;
+
+    const buyButton = document.createElement("button");
+    buyButton.innerText = "Comprar";
+
+    content.append(buyButton);
+
     shopContent.append(content);
+
+    buyButton.addEventListener("click", () => {
+        cart.push({
+            id: product.id,
+            productName: product.productName,
+            price: product.price,
+            quanty: product.quanty,
+            img: product.img,
+        });
+        console.log(cart);
+    });
 });
-
-const buyButton = document.createElement("button");
-buyButton.innerText = "Comparar";
-
-content.append(buyButton);
-buyButton.addEventListener("click", () => {
-    cart.push({
-        id: product.id,
-        productName: product.productName,
-        price: product.price,
-        quanty: 1
-    })
-})
