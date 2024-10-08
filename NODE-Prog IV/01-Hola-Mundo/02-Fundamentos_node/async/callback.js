d5d4609c82c53102cf8f8eeb04aa622c462ce26a
Marianela <135768183+MarianelaPurretta@users.noreply.github.com>
2024-10-08 15:22:48 -0300

    Clase 08 callbacks -> Cómo gestionar algo asíncrono

--- NODE-Prog IV/01-Hola-Mundo/02-Fundamentos_node/async/callback.js
+++ NODE-Prog IV/01-Hola-Mundo/02-Fundamentos_node/async/callback.js
@@ -1,4 +1,25 @@
-function soyAsincrona(){
-    console.log('Hola, soy una funcion asincrona');
+function hola(nombre, miCallback) {
+    setTimeout(function () {
+        console.log('Hola ' + nombre);
+        miCallback(nombre);
+    }, 1000);
+
 }
-soyAsincrona();
+
+function adios(nombre, otroCallback) {
+    setTimeout(function () {
+        console.log('Adios ' + nombre);
+        otroCallback();
+    }, 1000);
+}
+
+console.log('Iniciando el proceso...');
+hola('Carlos', function (nombre) {
+    adios(nombre, function () {
+        console.log('Terminando el proceso...');
+
+    });
+});
+
+//hola('Carlos', function () {});
+//adios('Carlos', function () {});
