02b044135329c7362f995b716f21234a7de11dbc
Marianela <135768183+MarianelaPurretta@users.noreply.github.com>
2024-10-10 14:49:13 -0300

    Clase 08 Proyecto nuevo Parte 2

---
+++ PYTHON - Lab IV/Juego_Tuc/constantes.py
@@ -0,0 +1,14 @@
+# Constantes.py
+
+# Dimensiones de pantalla
+SCREEN_WIDTH = 1700
+SCREEN_HEIGTH = 1000
+
+# Ruta de los assets
+ASSETS_PATH = 'C:/Users/Usuario/TECNICATURA-4TO SEM/Tecnicatura-4to-Semestre/PYTHON - Lab IV/Juego_Tuc/assets'
+
+# Rutas de los archivos
+IMPERIAL_MARCH_PATH = f'{ASSETS_PATH}/sonds/ Imperial March - kenobi.mp3'
+START_IMAGE_PATH = f'{ASSETS_PATH}/images/inicio/star.png'
+ESTRELLA_PATH = f'{ASSETS_PATH}/images/inicio/estrella.png'
+FONDO1_PATH = f'{ASSETS_PATH}/images/fondo1.jpg'
